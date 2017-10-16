74e153cb9c5052fb290d343a6c0b7b74a5e21d93
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-15 22:27:33 -0700

    minor tweaks

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var googleRequestUrl = "https://www.google.ca/search?q=" + movieName;
 var timeInMS = secToMS(time);
 
 var warning = (movieName, time) => {
-    return "**spoiler warning** we will be spoiling the plot of " + movieName + " in " + time + " seconds: \n";
+    return "**spoiler warning** we will be spoiling the plot of " + movieName + " in " + timeInMS + " seconds: \n";
 }
 
 //console.log('\x1b[31m%s\x1b[34m%s\x1b[0m', 'I am cyan','i am blue');
@@ -36,8 +36,8 @@ function Plot(title, details) {
 
 function secToMS(time) {
     timeInMS = parseInt(time) * 1000
-    if(isNaN(timeInMS)) {
-        time = readlineSync.question("Please enter valid wait time for spoiler(in seconds)");
+    if (isNaN(timeInMS)) {
+        time = readlineSync.question("Please enter valid wait time for spoiler(in seconds): ");
         timeInMS = secToMS(time);
     }
     return timeInMS;
@@ -46,21 +46,22 @@ function secToMS(time) {
 rp(imdbRequestUrl)
     .then(function (response) {
         let results = JSON.parse(response).results;
-        if (results.length) {
-            spoiler = new Plot(results[0].title, results[0].overview);
-            //After getting results display spoiler
-            console.log('\n\x1b[33m%s\x1b[0m', warning(movieName, time));
-            //wait for given time before giving google
-            setTimeout(function () {
-                console.log('\x1b[36m%s\x1b[0m',"Spoiler for the movie - " + movieName);
-                console.log('\x1b[36m%s\x1b[0m',"================================================\n")
+        //After getting results display spoiler
+        console.log('\n\x1b[33m%s\x1b[0m', warning(movieName, time));
+        setTimeout(function () {
+            if (results.length) {
+                spoiler = new Plot(results[0].title, results[0].overview);
+                
+                //wait for given time before giving google
+
+                console.log('\x1b[32m%s\x1b[0m', "Spoiler for the movie - " + movieName);
+                console.log('\x1b[32m%s\x1b[0m', "================================================\n")
                 console.log("Name: " + spoiler.title + "\n");
                 console.log("Plot: " + spoiler.details + "\n");
-            }, timeInMS);
-        } else {
-            console.log("Please enter a valid movie name")
-        }
-
+            } else {
+                console.log("Please enter a valid movie name")
+            }
+        }, timeInMS);
     })
     .catch(function (error) {
         console.log(error);
@@ -70,8 +71,8 @@ rp(imdbRequestUrl)
 request(googleRequestUrl, function (error, response, body) {
     if (!error) {
         var $ = cheerio.load(body);
-        console.log('\x1b[36m%s\x1b[0m',"Google results for the search term - " + movieName);
-        console.log('\x1b[36m%s\x1b[0m',"================================================\n")
+        console.log('\x1b[32m%s\x1b[0m', "Google results for the search term - " + movieName);
+        console.log('\x1b[32m%s\x1b[0m', "================================================\n")
         let links = $(".r a");
         links.each(function (i, link) {
             // get the href attribute of each link
